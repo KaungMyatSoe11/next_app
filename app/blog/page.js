6f49ab66e7c735898e9e3f74d9f33ce57e8e8d3e
Kaung Myat Soe <52688252+KaungMyatSoe11@users.noreply.github.com>
2023-09-27 10:48:50 +0630

    update
    Signed-off-by: Kaung Myat Soe <52688252+KaungMyatSoe11@users.noreply.github.com>

--- app/blog/page.js
+++ app/blog/page.js
@@ -1,9 +1,17 @@
+import view_photo from "@/assets/images/view1.jpg";
+import Image from "next/image";
+
 const Blog = () => {
-    return (
-        <div>
-            Blog page
-        </div>
-    );
-}
+  console.log(view_photo);
+  console.log(process.env.NEXT_PUBLIC_ANALYTICS_ID);
+  console.log(process.env.API_KEY);
+  return (
+    <div>
+      Blog page
+      <br />
+      <Image src={view_photo} alt="beach view photo"  />
+    </div>
+  );
+};
 
 export default Blog;
